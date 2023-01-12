2184f6a15a524f0294490fed204ee317a3fae61f
Ashutosh Jha <98846260+ashdude1401@users.noreply.github.com>
2023-01-13 01:33:00 +0530

    body-parser setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 //server
-//setting up the server 
+//All the external depandencies of same catogries like(middleware etc) should be together 
 const express = require("express");
+const bodyParser=require("body-parser");
+
 const {PORT}=require('./config/serverConfig');
 const setUpStartServer = async ()=>{
+    //setting up the server
     const app =express();
     app.listen(PORT,()=>{
         console.log("Server has started on PORT",PORT);
