4b5ea4ab7fb12b2cf36612b5a79927d4b2833d39
Ashutosh Jha <98846260+ashdude1401@users.noreply.github.com>
2023-01-13 00:57:11 +0530

    env variable setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 //server
+//setting up the server 
+const express = require("express");
+const {PORT}=require('./config/serverConfig');
+const setUpStartServer = async ()=>{
+    const app =express();
+    app.listen(PORT,()=>{
+        console.log("Server has started on PORT",PORT);
+    });
+}
+setUpStartServer();
+
