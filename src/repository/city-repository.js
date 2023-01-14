53e811fed2e76b8e89c9f881c03d3909b1de6d3b
Ashutosh Jha <98846260+ashdude1401@users.noreply.github.com>
2023-01-15 03:10:58 +0530

    City repositry completed (crud)

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,23 +1,57 @@
 //repositry are for intracting with models
 const {City} =require('../models/index');
 class cityRepository{
-    async createCity({name}){
+    //CRUD operation on city
+     async createCity({cityName}) {
         try{
-            City.create({name});
-        } catch(error){
-            throw(error);
+            //We have used await becase we making changes in server which out side the code and will take some time ,that is why we used asny promise based syntax
+           await City.create({name:cityName});//same as named argument in flutter
+        }
+        catch(error){
+            console.log("Something went wrong");
         }
     }
-    async deleteCity(cityId){
-        try{
-           await City.destroy({
-            where:{
-                id:cityId,
-            }
-           });
-        } catch(error){
-            throw(error);
+    async getCityById(cityId){
+        try {
+            //model.find one find the instances where your passed queries are meet 
+           const requiredCity= await City.findOne({
+                where:{
+                    id:cityId
+                }
+            });
+            console.log(requiredCity);
+          } 
+        catch (error) {
+            console.log("Something went wrong");
         }
+    }
+    
+    async updateCity(cityId,updatedData){
+        //model.update takes two parameter first is updatedData,second is query to tell where to update the data
+        try {
+            await City.update(updatedData,{
+                where:{
+                    id:cityId
+                },
+                returning:true
+            })
+        } catch (error) {
+            console.log("Something went wrong");
+        }
+       
+    }
+    async deleteCity(cityId){
+        try {
+             await City.destroy({
+            where:{
+                id:cityId
+            }
+        })
+        } catch (error) {
+            console.log("Something went wrong");
+        }
+       
+        
     }
 }
 module.exports=cityRepository;
